3a52678a658530791071bb4ab0b8e870b7541398
AndreD4 <andremai629@gmail.com>
2023-09-29 11:48:43 +0900

    examples of initialized and uninitialized variables

--- InitializingVariables/main.cpp
+++ InitializingVariables/main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
+// This program will calculate the area of a room in square feet
+
 int main() 
 {
-  cout << "Default Template" << endl;    
+//    unintialized variables
+//    int age;
+//    cout << age << endl; 
+
+//    initialized variables
+    int age {21};
+    cout << age << endl;
+   
   return 0;
 }
