27a4aa716c6989a1ae05f41db0a893dbc3e4df36
AndreD4 <andremai629@gmail.com>
2023-09-29 11:58:09 +0900

    Example of initializing variables

--- InitializingVariables/main.cpp
+++ InitializingVariables/main.cpp
@@ -11,8 +11,22 @@ int main()
 //    cout << age << endl; 
 
 //    initialized variables
-    int age {21};
-    cout << age << endl;
-   
+//    int age {21};
+//    cout << age << endl;
+    
+    int room_width {0};
+    int room_length {0};
+    
+    cout << "Enter the width of the room: ";
+    cin >> room_width;
+    
+    cout << "Enter the length of the room: ";
+    cin >> room_length;
+    
+    
+    cout << "The area of the room is " << room_width * room_length << " sqaure feet" << endl;
+    
+    
+    
   return 0;
 }
